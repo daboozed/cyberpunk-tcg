05a579b96807fb1aaa872682baf233da1f382442
daboozed <91267160+daboozed@users.noreply.github.com>
2026-04-24 11:30:32 -0400

    Update PlayerArea.jsx

--- src/components/game/PlayerArea.jsx
+++ src/components/game/PlayerArea.jsx
@@ -1,7 +1,7 @@
   import { useState } from "react";
   import UnitWithGear from "./UnitWithGear";
   import GigDice from "./GigDice";
-  import { getAvailableEddies, getAvailableLegendEddies, PHASES } from "@/lib/gameEngine";
+  import { getAvailableEddies, getAvailableLegendEddies, PHASES } from "@/lib/game/gameEngine";
   import { LEGEND_BACK, CARD_BACK } from "@/lib/cardPool";
   import CardHoverPreview from "./CardHoverPreview";
   import { cn } from "@/lib/utils";
