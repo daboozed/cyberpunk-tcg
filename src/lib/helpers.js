bdefe486e17b8a0cd63a50f59509b408b9d52dbc
daboozed <91267160+daboozed@users.noreply.github.com>
2026-04-24 08:42:52 -0400

    Update helpers.js

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,1 +1,8 @@
 
+export function getAvailableEddies(player){...}
+export function getAvailableLegendEddies(player){...}
+export function spendEddies(player, amount){...}
+export function getUnitPower(unit){...}
+export function calcPower(unit){...}
+
+
