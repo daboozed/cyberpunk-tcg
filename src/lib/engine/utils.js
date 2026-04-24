3d7189ad1438cff7d3c01c465a16a8205a76516c
daboozed <91267160+daboozed@users.noreply.github.com>
2026-04-24 13:26:13 -0400

    Update utils.js
    Fisher Yates shuffle

--- src/lib/engine/utils.js
+++ src/lib/engine/utils.js
@@ -1,5 +1,12 @@
 export function shuffle(arr) {
-  return [...arr].sort(() => Math.random() - 0.5);
+  const copy = [...arr];
+
+  for (let i = copy.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [copy[i], copy[j]] = [copy[j], copy[i]];
+  }
+ 
+  return copy;
 }
 
 export function clone(o) {
