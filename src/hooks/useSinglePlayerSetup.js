b3ce492c19e002cf32ee6600efa26332f8f3c705
Eric Matta <elmatta@gmail.com>
2026-05-07 19:36:27 -0400

    Wire single player setup hook

--- src/hooks/useSinglePlayerSetup.js
+++ src/hooks/useSinglePlayerSetup.js
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { buildCustomDeck } from "@/lib/cardPool";
-import { useSinglePlayerSetup } from "@/hooks/useSinglePlayerSetup";
 import {
   createInitialState,
   setupGame,
